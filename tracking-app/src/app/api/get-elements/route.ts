74528b02b1f649dd1a01d0e604538bac044c4035
AleexCaires <tiagoaleex98@gmail.com>
2025-04-30 21:03:26 +0700

    Changes to logic to Allow Laithwaites to have a different Structure

--- tracking-app/src/app/api/get-elements/route.ts
+++ tracking-app/src/app/api/get-elements/route.ts
@@ -9,22 +9,35 @@ export async function GET() {
     const elements = await collection.find({}).toArray();
 
     // Include `experienceName` in the response
-    const formattedElements = elements.map((element) => ({
-      _id: element._id,
-      client: element.client,
-      dateCreated: element.dateCreated,
-      experienceName: element.experienceName,
-      events: element.events,
-    }));
+    const formattedElements = elements.map((element) => {
+      const isLaithwaites = element.client === "Laithwaites";
+
+      const formattedEvents = element.events.map((event: any) => {
+        if (isLaithwaites && event.event === "targetClickEvent") {
+          return {
+            eventAction: event.eventData.click.clickAction,
+            eventCategory: event.eventData.click.clickLocation,
+            eventLabel: event.eventData.click.clickText,
+            eventSegment: "", // Adobe-specific events do not have eventSegment
+          };
+        }
+        return event;
+      });
+
+      return {
+        _id: element._id,
+        client: element.client,
+        dateCreated: element.dateCreated,
+        experienceName: element.experienceName,
+        events: formattedEvents,
+      };
+    });
 
     return new Response(JSON.stringify({ elements: formattedElements }), {
       status: 200,
       headers: { "Content-Type": "application/json" },
     });
   } catch (error: any) {
-    return new Response(
-      JSON.stringify({ message: `Failed to fetch elements: ${error.message}` }),
-      { status: 500 }
-    );
+    return new Response(JSON.stringify({ message: `Failed to fetch elements: ${error.message}` }), { status: 500 });
   }
 }
