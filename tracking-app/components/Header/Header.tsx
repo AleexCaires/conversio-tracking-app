72cad922a36d4c03aa1d4edba54e713c5a880976
AleexCaires <tiagoaleex98@gmail.com>
2025-05-30 14:42:16 +0100

    Logic to hide Btn when needed and small text tweaks

--- tracking-app/components/Header/Header.tsx
+++ tracking-app/components/Header/Header.tsx
@@ -3,10 +3,13 @@
 import React from "react";
 import Image from "next/image";
 import { Nav, HeaderWrapper, Logo, NavList, NavItemLink, NavItem } from "./Header.styles";
+import { usePathname } from "next/navigation"; // Import usePathname
 
-const navItems = [{ name: "See all", href: "/historyComp" }];
+const navItems = [{ name: "Saved Events", href: "/historyComp" }];
 
 const Header = () => {
+  const pathname = usePathname(); // Get current pathname
+
   return (
     <HeaderWrapper>
       <Nav>
@@ -14,11 +17,17 @@ const Header = () => {
           <Image src="/conversioLogo.png" alt="Conversio Logo" width={120} height={40} priority />
         </Logo>
         <NavList>
-          {navItems.map((item) => (
-            <NavItem key={item.name}>
-              <NavItemLink href={item.href}>{item.name}</NavItemLink>
-            </NavItem>
-          ))}
+          {navItems.map((item) => {
+            // Conditionally render the item
+            if (item.href === "/historyComp" && pathname === "/historyComp") {
+              return null; // Don't render if on /historyComp page
+            }
+            return (
+              <NavItem key={item.name}>
+                <NavItemLink href={item.href}>{item.name}</NavItemLink>
+              </NavItem>
+            );
+          })}
         </NavList>
       </Nav>
     </HeaderWrapper>
